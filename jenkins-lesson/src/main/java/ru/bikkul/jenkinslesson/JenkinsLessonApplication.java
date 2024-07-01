7c11939cec873c5c83374887b7c618cbdfbbaea5
Alexander Bikkulov <quadalus@yandex.ru>
2024-07-01 12:10:41 +0300

    feat: init commit

---
+++ jenkins-lesson/src/main/java/ru/bikkul/jenkinslesson/JenkinsLessonApplication.java
@@ -0,0 +1,13 @@
+package ru.bikkul.jenkinslesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsLessonApplication.class, args);
+    }
+
+}
